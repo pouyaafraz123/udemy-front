e25a933ba9773892ea47deb4b706e46854534b90
pouyaafraz123 <pouyaafraz123@gmail.com>
2022-08-10 15:27:19 +0430

    f2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "jquery/dist/jquery.min";
 import "@popperjs/core/dist/umd/popper.min";
-import "bootstrap/dist/css/bootstrap.rtl.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
